c695082d6b61353d5dc423da04e475e34c1502e9
Wajih Ghali <wajih.elghali@gmail.com>
2022-12-08 10:44:21 +0100

    first commit ecommerce front

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import styled from 'styled-components';
+import SearchIcon from '@material-ui/icons/Search';
+import Badge from '@material-ui/core/Badge';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import { mobile } from '../resposive';
+
+const Container = styled.div`
+  height: 60px;
+  ${mobile({ height: '50px' })}
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({ padding: '10px 0 px' })}
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Input = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  border: none;
+  ${mobile({width: "50px"})}
+
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-item: center;
+  justify-content: flex-end;
+  ${mobile({flex:2 ,jusitfyContent: "center"})}
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+  ${mobile({display: "none"})}
+
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  ${mobile({fontSize: "24px"})}
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  ${mobile({fontSize: "12px", marginLeft:"10px" })}
+`;
+
+function Navbar() {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input placeholder= "Search" />
+            <SearchIcon style={{ color: 'gray', fontSize: '16px' }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>LAMA.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>Register</MenuItem>
+          <MenuItem>Sign in</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+}
+
+export default Navbar;
